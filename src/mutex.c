85d517c26ce2bf72de2b6c86df54ca7abdaa3428
Alexandre <alraffin@ipb.fr>
2015-04-15 08:02:05 +0200

    retrait du pointeur void* dans la structure mutex, reste quelques corrections à effectuer

--- src/mutex.c
+++ src/mutex.c
@@ -6,79 +6,78 @@
 // pour opérations atomiques (nécessaire si on gère plusieurs coeurs) -> C11 (prof) ou openPA (perso).
 
 
-static struct mutex_struct mutex_struct = {.mutex=NULL, .state=0, .attr=MUTEX_NORMAL};
+static struct mutex_t mutex = {.state=0, .attr=MUTEX_NORMAL};
 
 
-int mutex_init(mutex_t * mutex, const enum attr * attr){
+int mutex_init(struct mutex_t * mutex, const enum attr * attr){
   
   if (attr != NULL){
     if (*attr !=  MUTEX_NORMAL && *attr != MUTEX_RECURSIVE && *attr != MUTEX_ERRORCHECK) // attr n'est pas l'une des 3 valeurs supportées, ni NULL
       return -1;
     
     else // attr != NULL et est l'une des 3 valeurs supportées
-      mutex_struct.attr = *attr;
+      mutex->attr = *attr;
   }    
 
   else  // attr == NULL    
-    mutex_struct.attr = MUTEX_NORMAL;
+    mutex->attr = MUTEX_NORMAL;
 
-  mutex_struct.mutex = mutex; 
-  mutex_struct.state = 2; // déverrouillé
+  mutex->state = 2; // déverrouillé
   //printf("mutex_init\n");
   return 0;
 }
 
-int mutex_destroy(mutex_t * mutex){
-  // && mutex_struct.attr == MUTEX_ERRORCHECK à laisser ? ou tout le tps check ?
-  if(mutex_struct.state == 1 && mutex_struct.attr == MUTEX_ERRORCHECK){ // essai de détruire un mutex verrouillé, comportement indéfini
+int mutex_destroy(struct mutex_t * mutex){
+  // && mutex_t.attr == MUTEX_ERRORCHECK à laisser ? ou tout le tps check ?
+  if(mutex->state == 1 && mutex->attr == MUTEX_ERRORCHECK){ // essai de détruire un mutex verrouillé, comportement indéfini
     printf("Attention, vous avez essayé de détruire un mutex verrouillé\n");
     return -1;
   }
  
-  mutex_struct.state = 0; // détruire = désinitialiser
+  mutex->state = 0; // détruire = désinitialiser
   return 0;
 }
 
-int mutex_lock(mutex_t * mutex){
-  if(mutex_struct.attr ==  MUTEX_ERRORCHECK && mutex_struct.state == 1){ // déjà verrouillé
+int mutex_lock(struct mutex_t * mutex){
+  if(mutex->attr ==  MUTEX_ERRORCHECK && mutex->state == 1){ // déjà verrouillé
     printf("Tentative de verrouillage de mutex déjà verrouillé.\n");
-    while(mutex_struct.state == 1);
+    while(mutex->state == 1);
     return -2;
   }
 
-  else if(mutex_struct.attr ==  MUTEX_ERRORCHECK && mutex_struct.state == 0){ // non initialisé
+  else if(mutex->attr ==  MUTEX_ERRORCHECK && mutex->state == 0){ // non initialisé
     printf("Tentative de verrouillage de mutex non initialisé.\n");
     return -1;
   }
   //printf("mutex_lock\n");
   
-  while(mutex_struct.state == 1); // on bloque la ressource (pb : attente active -> 100% de proc)
-  mutex_struct.state = 1; // on verrouille
+  while(mutex->state == 1); // on bloque la ressource (pb : attente active -> 100% de proc)
+  mutex->state = 1; // on verrouille
   return 0;
 }
 
-int mutex_trylock(mutex_t * mutex){
-  if(mutex_struct.state == 1) // si verrouillé
+int mutex_trylock(struct mutex_t * mutex){
+  if(mutex->state == 1) // si verrouillé
     return 0;
 
   else return mutex_lock(mutex);   
 
 }
 
-int mutex_unlock(mutex_t * mutex){
+int mutex_unlock(struct mutex_t * mutex){
   
-  if(mutex_struct.attr == MUTEX_ERRORCHECK && mutex_struct.state == 2){ // déjà déverrouillé
+  if(mutex->attr == MUTEX_ERRORCHECK && mutex->state == 2){ // déjà déverrouillé
     printf("Tentative de déverrouillage de mutex déjà déverrouillé.\n");
     return -2;
   }
 
-  else if(mutex_struct.attr == MUTEX_ERRORCHECK && mutex_struct.state == 0){ // non initialisé
+  else if(mutex->attr == MUTEX_ERRORCHECK && mutex->state == 0){ // non initialisé
     printf("Tentative de déverouillage de mutex non initialisé.\n");
     return -1;
   }
 
   else{
-    mutex_struct.state = 2;
+    mutex->state = 2;
     return 0;
   }
 }
--- src/mutex.h
+++ src/mutex.h
@@ -5,14 +5,14 @@
 
 /* identifiant de mutex
  */
-typedef void * mutex_t;
+//typedef void * mutex_t;
 
 enum attr {MUTEX_NORMAL, MUTEX_RECURSIVE, MUTEX_ERRORCHECK};
 
 /* structure de mutex
  */
-struct mutex_struct{
-  mutex_t mutex;
+struct mutex_t{
+  //mutex_t mutex;
   int state; // 0 = non initialisé, 1 = verrouillé, 2 = déverrouillé
   enum attr attr;
 };
@@ -21,13 +21,13 @@ struct mutex_struct{
 /* Créer un nouveau mutex
    Renvoie 0 en cas de succès, -1 en cas d'erreur
 */
-int mutex_init(mutex_t * mutex, const enum attr * attr);
+int mutex_init(struct mutex_t * mutex, const enum attr * attr);
 
 
 /* Détruit le mutex référencé par mutex
    Retourne 0 si tout s'est bien passé, -1 en cas d'erreur
 */
-int mutex_destroy(mutex_t * mutex);
+int mutex_destroy(struct mutex_t * mutex);
 
 
 /* Verrouille le mutex
@@ -35,13 +35,13 @@ int mutex_destroy(mutex_t * mutex);
    -1 si le mutex n'était pas initialisé
    -2 si le mutex était déjà verrouillé
 */
-int mutex_lock(mutex_t * mutex);
+int mutex_lock(struct mutex_t * mutex);
 
 
 /* Verrouille le mutex.
    Retourne immédiatement si le mutex est déjà verrouillé
 */
-int mutex_trylock(mutex_t * mutex);
+int mutex_trylock(struct mutex_t * mutex);
 
 
 /* Déverrouille le mutex  
@@ -49,14 +49,14 @@ int mutex_trylock(mutex_t * mutex);
    -1 si le mutex n'était pas initialisé
    -2 si le mutex était déjà déverrouillé
 */
-int mutex_unlock(mutex_t * mutex);
+int mutex_unlock(struct mutex_t * mutex);
 
 #else /* USE_PTHREAD */
 
 /* Si on compile avec -DUSE_PTHREAD, ce sont les pthreads qui sont utilisés */
 #include <sched.h>
 #include <pthread.h>
-#define mutex_t pthread_mutex_t
+#define struct mutex_t pthread_struct mutex_t
 #define mutex_attr_t pthread_mutex_attr_t
 #define MUTEX_NORMAL PTHREAD_MUTEX_NORMAL
 #define MUTEX_RECURSIVE PTHREAD_MUTEX_RECURSIVE
@@ -64,7 +64,7 @@ int mutex_unlock(mutex_t * mutex);
 #define mutex_init pthread_mutex_init
 #define mutex_destroy pthread_mutex_destroy
 #define mutex_lock pthread_mutex_lock
-#define mutex_trylock pthread_mutex_trylock
+#define struct mutex_trylock pthread_struct mutex_trylock
 #define mutex_unlock pthread_mutex_unlock
 #define thread_self pthread_self
 
